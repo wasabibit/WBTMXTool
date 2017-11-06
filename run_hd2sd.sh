64ce306c85e1cd75def0f7961e32f30da7e4b9e2
wasabibit@gmail.com <wasabibit@dhcp-10-15-188-91.wireless.pacific.edu>
2017-11-06 12:41:57 -0800

    init checkin

---
+++ run_hd2sd.sh
@@ -0,0 +1,1 @@
+./WBTMXTool -in sample-hd.tmx -out sample.tmx -scale 0.5 -suffixForHD -hd -suffixAction remove
---
+++ run_sd2hd.sh
@@ -0,0 +1,1 @@
+./WBTMXTool -in sample.tmx -out sample-hd.tmx -scale 2.0 -suffixForHD -hd -suffixAction add
